2c784e5b37e4237701e1ac8a69241954b11d0658
Richard Xiong <richardx366@gmail.com>
2025-02-15 20:00:18 -0500

    fix

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -3,7 +3,11 @@ import { RequestHandler } from 'express';
 export const sentMessage: RequestHandler = async (req, res) => {
   const { payload } = req.body;
 
-  const message = payload.object.chat_message;
+  const message = payload?.object?.chat_message;
+
+  if (!message) {
+    return res.json({});
+  }
 
   console.log(message.sender_name, message.message_content);
 
