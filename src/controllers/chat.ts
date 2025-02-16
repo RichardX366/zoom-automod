6a6efb39b269a4355f08c42300ba85a409cca45f
Richard Xiong <richardx366@gmail.com>
2025-02-15 20:08:12 -0500

    challenge

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -1,13 +1,20 @@
 import { RequestHandler } from 'express';
+import hmacSHA512 from 'crypto-js/hmac-sha512';
 
 export const sentMessage: RequestHandler = async (req, res) => {
-  const { payload } = req.body;
+  const { payload, event } = req.body;
 
-  const message = payload?.object?.chat_message;
+  if (event === 'endpoint.url_validation') {
+    const { plainToken } = payload;
+    const encryptedToken = hmacSHA512(
+      plainToken,
+      process.env.ZOOM_SECRET || '',
+    ).toString();
 
-  if (!message) {
-    return res.json({});
+    return res.json({ plainToken, encryptedToken });
   }
+
+  const message = payload.object.chat_message;
 
   console.log(message.sender_name, message.message_content);
 
